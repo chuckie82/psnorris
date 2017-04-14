cbb4bb9f2375674bf00aadfb3646692e2ddf468a
monarin <monarin@gmail.com>
2017-04-14 13:53:50 -0700

    Add function calls for phenix.refine and coot.

---
+++ psnorris/viz.py
@@ -0,0 +1,17 @@
+from subprocess import call
+def launch_phenix_refine(mtzfile, pdbfile):
+  """
+  The function takes a merged mtz and pdb files, then run phenix.refine for
+  1 cycle and output the refined map for coot.
+  """
+  call(['phenix.refine', mtzfile, pdbfile, 'main.number_of_macro_cycles=1', \
+  'xray_data.r_free_flags.generate=True', 'nproc=16', 'strategy=none'])
+  
+
+def lanch_coot(mtzfile, pdbfile)
+  """
+  The function takes a refined mtz and pdb files from phenix.refine, then
+  call coot to show map.
+  """
+  call(['coot','--auto',mtzfile,'--pdb',pdbfile])
+  

f5aeac840c4da8a51054e5f6d3284fd4743c680e
monarin <monarin@gmail.com>
2017-04-14 14:45:45 -0700

    Add test and data folders

--- psnorris/viz.py
+++ psnorris/viz.py
@@ -1,4 +1,6 @@
 from subprocess import call
+import sys
+
 def launch_phenix_refine(mtzfile, pdbfile):
   """
   The function takes a merged mtz and pdb files, then run phenix.refine for
@@ -8,10 +10,16 @@ def launch_phenix_refine(mtzfile, pdbfile):
   'xray_data.r_free_flags.generate=True', 'nproc=16', 'strategy=none'])
   
 
-def lanch_coot(mtzfile, pdbfile)
+def lanch_coot(mtzfile, pdbfile):
   """
   The function takes a refined mtz and pdb files from phenix.refine, then
   call coot to show map.
   """
   call(['coot','--auto',mtzfile,'--pdb',pdbfile])
   
+
+if __name__ == "__main__":
+  if sys.argv[1:]:
+    mtzfile = sys.argv[1]
+    pdbfile = sys.argv[2]
+    

717f63ace8a7b2cce1ff133007adc6f733040f47
monarin <monarin@gmail.com>
2017-05-03 15:03:28 -0700

    Add findSpots

--- psnorris/cctbxPlay.py
+++ psnorris/cctbxPlay.py
@@ -2,33 +2,31 @@ from subprocess import call
 import sys, os
 
 class cctbxPlayMan:
-  def __init__(self, exp=None, runNo=None, trialNo=None, targetPhil=None, outputDir=None, replaceDir=False, qName='psanaq', nproc=12):
+  def __init__(self, exp=None, runNo=None, trialNo=None, targetPhil=None, outputDir=None, qName='psanaq', nproc=12):
     self.prog = "Cctbx.xfel wrapper for autosfx"
     self.exp = exp
     self.runNo = runNo
     self.trialNo = trialNo
     self.targetPhil = targetPhil
     self.outputDir = outputDir
-    self.replaceDir = replaceDir
+    self.qName = qName
     self.nproc = nproc
+    self.playground = os.path.join(self.outputDir, 'r{:04d}'.format(self.runNo), '{:03d}'.format(self.trialNo))
     if exp is None or runNo is None or trialNo is None or targetPhil is None:
       print "Please provide experiment name, run no., and target. Exit. Good Bye."
       return 0
       
-  def buildPlayground(self):
+  def buildPlayground(self, replaceDir=False):
     """
     Create folders for the experiment and run.
     """
-    print "cctbxPlay just did this:"
-    self.playground = os.path.join(self.outputDir, 'r{:04d}'.format(self.runNo), '{:03d}'.format(self.trialNo))
-    if self.replaceDir: 
+    if replaceDir: 
       cmd = ['rm', '-rf', self.playground]
       call(cmd)
-      print cmd
-    cmd = ['cxi.mpi_submit', 'dry_run=True', 'output.output_dir='+self.outputDir, \
-        'input.experiment='+str(self.exp), 'input.run_num='+str(self.runNo), 'input.trial='+str(self.trialNo)]
-    call(cmd)
-    print cmd
+      print "Removing", self.playground
+    print "Creating", self.playground
+    call(['mkdir','-p', os.path.join(self.playground,'out')])
+    call(['mkdir','-p', os.path.join(self.playground,'stdout')])
     return 1
         
   
@@ -36,9 +34,29 @@ class cctbxPlayMan:
     """
     Given all the parameters and playground created, run spotfinding using bsub.
     """
-    q_cmd = '"cctbx.xfel.xtc_process input.experiment='+self.exp+' input.run_num='+str(self.runNo)
+    q_cmd = 'mpirun cctbx.xfel.xtc_process input.experiment='+self.exp+' input.run_num='+str(self.runNo)
     q_cmd += ' output.logging_dir='+os.path.join(self.playground,'stdout')+' output.output_dir='+os.path.join(self.playground,'out')
-    q_cmd += ' '+self.targetPhil+'"'
-    print "cctbxPlay just did this:"
-    print q_cmd
+    q_cmd += ' '+self.targetPhil
+    cmd = ['bsub', '-n', str(self.nproc), '-q', self.qName, '-o', os.path.join(self.playground,'stdout/log.out'), q_cmd]
+    call(cmd)
+    return 1
+    
+  def isDone(self):
+    """
+    From a stateless bsub job launched in any of the functions here, 
+    look for log file and see if it's done.
+    """
+    qFlag = None
+    with open(os.path.join(self.playground,'stdout','log.out'), 'r') as f:
+      from cctbxTools import tail
+      qFlag = tail(f)[3].strip()
+      
+    print qFlag=='Successfully completed.'
+      
+      
+    
+    
+    
+    
+    
     

7afea69243b806f00969b40820283b178d3846ee
monarin <monarin@gmail.com>
2017-06-27 16:25:18 -0700

    Remove time limit.

--- psnorris/replay_cctbx.py
+++ psnorris/replay_cctbx.py
@@ -1,8 +1,8 @@
 from cctbxPlay import cctbxPlayMan
 import time
 
-def timeout(startTime, limitSec, cPMan, playActivity):
-  while time.time() - startTime < limitSec:
+def timeout(cPMan, playActivity):
+  while True:
     results = cPMan.isDone(playActivity=playActivity)
     print results
     if results[0]:  
@@ -11,22 +11,20 @@ def timeout(startTime, limitSec, cPMan, playActivity):
       time.sleep(5)
   
 if __name__ == '__main__':
-  # Limit polling isDone in seconds
-  limitSec = 200000
   # Initialize cctbxPlayMan
   cPMan = cctbxPlayMan(exp='cxid9114', runNo=96, trialNo=7, targetPhil='target.phil', outputDir='dials', nproc=72)
   # Create folder structure
   cPMan.buildPlayground(replaceDir=True)
   # Find spots
   cPMan.findSpots()
-  timeout(time.time(), limitSec, cPMan, 'findSpots')
+  timeout(cPMan, 'findSpots')
   # Index
   cPMan.doIndex()
-  timeout(time.time(), limitSec, cPMan, 'doIndex')
+  timeout(cPMan, 'doIndex')
   # Integrate
   cPMan.doIntegrate()
-  timeout(time.time(), limitSec, cPMan, 'doIntegrate')
+  timeout(cPMan, 'doIntegrate')
   # Merging
   cPMan.doMerge()
-  timeout(time.time(), limitSec, cPMan, 'doMerge')
+  timeout(cPMan, 'doMerge')
   

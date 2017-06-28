b5bae4c310ea52a4f0855170942bc4afd6d38595
monarin <monarin@gmail.com>
2017-06-27 18:59:27 -0700

    Add automatic MR-Phenix.Refine-Coot function.

--- psnorris/phenixPlay.py
+++ psnorris/phenixPlay.py
@@ -1,42 +1,33 @@
-from subprocess import call
-import sys
+import subprocess
+import sys, os
 
-def launch_phaser(mtzfile, pdbfile, **kwargs):
-  """
-  The function takes an mtzfile and the model (pdbfile) with
-  corresponding phaser parameters (automatically constructed) 
-  and performs MR then output PHASER.1.mtz and PHASER.1.pdb
-  on the current directory. 
-  
-  See /reg/common/package/phenix/phenix-1.10.1-2155/modules/phaser/phaser/phenix_interface/__init__.params
-  for acceptable keywords.
-  """
-  params = [str(key)+'='+str(val) for key, val in kwargs.iteritems()]
-  tmp = ['phenix.phaser', 'phaser.hklin='+mtzfile, 'phaser.model='+pdbfile] + params
-  print tmp
-  #call(['phenix.phaser', 'phaser.hklin='+mtzfile, 'phaser.model='+pdbfile])
-
-def launch_phenix_refine(mtzfile, pdbfile):
-  """
-  The function takes an mtz and pdb files (e.g. PHASER.1.mtz and PHASER.1.pdb), 
-  then runs phenix.refine and outputs the refined map for coot.
-  """
-  #call(['phenix.refine', mtzfile, pdbfile, 'main.number_of_macro_cycles=1', \
-  #'xray_data.r_free_flags.generate=True', 'nproc=12', 'strategy=none'])
-  
-  call(['phenix.refine', '--overwrite', mtzfile, pdbfile, 'xray_data.r_free_flags.generate=True', 'nproc=12'])
-  
-
-def lanch_coot(mtzfile, pdbfile):
-  """
-  The function takes a refined mtz and pdb files from phenix.refine, then
-  call coot to show map.
-  """
-  call(['coot','--auto',mtzfile,'--pdb',pdbfile])
-  
-
-if __name__ == "__main__":
-  if sys.argv[1:]:
-    mtzfile = sys.argv[1]
-    pdbfile = sys.argv[2]
+class phenixPlayMan:
+  def launch_phaser(self, mtzfile, pdbfile, *args):
+    """
+    The function takes an mtzfile and the model (pdbfile) with
+    corresponding phaser parameters (automatically constructed) 
+    and performs MR then output PHASER.1.mtz and PHASER.1.pdb
+    on the current directory. 
     
+    See /reg/common/package/phenix/phenix-1.10.1-2155/modules/phaser/phaser/phenix_interface/__init__.params
+    for acceptable keywords.
+    """
+    subprocess.call(['phenix.phaser', 'phaser.hklin='+mtzfile, 'phaser.model='+pdbfile] + list(args))
+
+  def launch_phenix_refine(self, mtzfile, pdbfile, *args):
+    """
+    The function takes an mtz and pdb files (e.g. PHASER.1.mtz and PHASER.1.pdb), 
+    then runs phenix.refine and outputs the refined map for coot.
+    """
+    subprocess.call(['phenix.refine', '--overwrite', mtzfile, pdbfile] + list(args))
+    
+
+  def launch_coot(self, mtzfile, pdbfile):
+    """
+    The function takes a refined mtz and pdb files from phenix.refine, then
+    call coot to show map.
+    """
+    subprocess.Popen(['coot','--auto',mtzfile,'--pdb',pdbfile])
+    
+  
+
--- psnorris/replay_cctbx.py
+++ psnorris/replay_cctbx.py
@@ -1,5 +1,6 @@
 from cctbxPlay import cctbxPlayMan
-import time
+from phenixPlay import phenixPlayMan
+import time, glob, shutil
 
 def timeout(cPMan, playActivity):
   while True:
@@ -9,7 +10,15 @@ def timeout(cPMan, playActivity):
       break
     else:
       time.sleep(5)
-  
+
+def getLatestRun(moduleName):
+  runList = glob.glob(moduleName+'*')
+  if runList:
+    latestRun = max([int(runName.split(moduleName)[-1].split('_')[0]) for runName in runList])
+  else:
+    latestRun = 0
+  return latestRun
+    
 if __name__ == '__main__':
   # Initialize cctbxPlayMan
   cPMan = cctbxPlayMan(exp='cxid9114', runNo=96, trialNo=7, targetPhil='target.phil', outputDir='dials', nproc=72)
@@ -27,4 +36,20 @@ if __name__ == '__main__':
   # Merging
   cPMan.doMerge()
   timeout(cPMan, 'doMerge')
+
+  # Now ready to run phenix MR-Refine-Coot
+  latestPrimeRun = getLatestRun('Prime_Run_')
+  pdbFile = '1dpx_clean.pdb'
+  if latestPrimeRun:
+    phenixPM = phenixPlayMan()
+    # MR by Phaser
+    latestPostrefCycle = getLatestRun('Prime_Run_'+str(latestPrimeRun)+'/postref_cycle_')
+    phenixPM.launch_phaser('Prime_Run_'+str(latestPrimeRun)+'/postref_cycle_'+str(latestPostrefCycle)+'_merge.mtz', 
+        pdbFile, 'phaser.model_rmsd=0.8')
+    # Open Coot for monitoring
+    shutil.copy('PHASER.1.mtz', 'PHASER.1_refine_001.mtz')
+    shutil.copy('PHASER.1.pdb', 'PHASER.1_refine_001.pdb')
+    phenixPM.launch_coot('PHASER.1_refine_001.mtz', 'PHASER.1_refine_001.pdb')
+    # Phenix.refine
+    phenixPM.launch_phenix_refine('PHASER.1.mtz', 'PHASER.1.pdb', 'xray_data.r_free_flags.generate=True', 'nproc=12')
   
